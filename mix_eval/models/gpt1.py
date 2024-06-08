e1ed32a09f5031848878c733648c656a395e448f
Cedaros <64960661+Ce-daros@users.noreply.github.com>
2024-06-08 16:28:28 +0800

    Create gpt1.py

---
+++ mix_eval/models/gpt1.py
@@ -0,0 +1,19 @@
+from mix_eval.models.base import BaseModel
+from mix_eval.api.registry import register_model
+
+@register_model("gpt1")
+class GPT_1(BaseModel):
+    def __init__(self, args):
+        super().__init__(args)
+        self.model_name = "openai-community/openai-gpt"
+        self.attn_implementation = None # If use default, set to None
+
+        self.model = self.build_model().float32()
+        self.model_max_len = self.model.config.max_position_embeddings 
+        self.tokenizer = self.build_tokenizer()
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.max_input_length_closeend = min(
+            self.model_max_len,
+            self.max_input_length
+        ) - self.closeended_max_new_tokens
+
